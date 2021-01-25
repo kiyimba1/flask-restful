40d868ad56bf6b5a25afb01b59dea46c5014f9e4
Edwin Kiyimba <edwineddie54@gmail.com>
2021-01-25 23:19:07 +0300

    First Impression

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos [todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
 
 
 class HelloWorld(Resource):
@@ -10,6 +20,7 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
 if __name__ == '__main__':
